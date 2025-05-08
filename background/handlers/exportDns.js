c488da4eda0dc67f4fb75daf5a32e54e263753ed
Joshua Miner <joshua.miner@20i.com>
2025-05-08 17:29:14 +0100

    Added Call to Server

--- background/handlers/exportDns.js
+++ background/handlers/exportDns.js
@@ -1,4 +1,4 @@
-import { fetchJson } from '../utils/fetchData.js';
+import { fetchJson } from "../utils/fetchData.js";
 //import { combineResponses } from '../utils/combineResponses.js';
 
 export async function handleExportDns(message, sender) {
@@ -6,26 +6,45 @@ export async function handleExportDns(message, sender) {
   const timeout = setTimeout(() => controller.abort(), 15000);
 
   const [allPackages] = await Promise.all([
-    fetchJson("https://my.20i.com/a/package?fields%5B%5D=names&fields%5B%5D=id", {
-      credentials: "include",
-      signal: controller.signal,
-    })
+    fetchJson(
+      "https://my.20i.com/a/package?fields%5B%5D=names&fields%5B%5D=id",
+      {
+        credentials: "include",
+        signal: controller.signal,
+      }
+    ),
   ]);
 
   clearTimeout(timeout);
 
+  const [sortedPackages] = await Promise.all([
+    fetchJson(
+      "https://dns-exporter.joshuaminer.uk/build_domain_package_list.py",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json",
+        },
+        body: JSON.stringify(allPackages),
+      }
+    ),
+  ]);
+
+  console.log(sortedPackages);
+
+  /* 
   const selectedPackages = allPackages.reduce((acc, item) => {
     acc[item.id] = item.names;
     return acc;
   }, {});
 
-  console.log(selectedPackages);
+  console.log(selectedPackages); */
   //const combined = combineResponses(dnsRecords, metadata);
 
-//  chrome.runtime.sendMessage({
-//    type: 'EXPORT_DNS_COMPLETE',
-//    payload: { success: true },
-//  });
+  //  chrome.runtime.sendMessage({
+  //    type: 'EXPORT_DNS_COMPLETE',
+  //    payload: { success: true },
+  //  });
 
-  return { status: 'done' };
+  return { status: "done" };
 }
--- dist/background/index.js
+++ dist/background/index.js
@@ -12,17 +12,28 @@ async function handleExportDns(message, sender) {
   const controller = new AbortController();
   const timeout = setTimeout(() => controller.abort(), 15e3);
   const [allPackages] = await Promise.all([
-    fetchJson("https://my.20i.com/a/package?fields%5B%5D=names&fields%5B%5D=id", {
-      credentials: "include",
-      signal: controller.signal
-    })
+    fetchJson(
+      "https://my.20i.com/a/package?fields%5B%5D=names&fields%5B%5D=id",
+      {
+        credentials: "include",
+        signal: controller.signal
+      }
+    )
   ]);
   clearTimeout(timeout);
-  const selectedPackages = allPackages.reduce((acc, item) => {
-    acc[item.id] = item.names;
-    return acc;
-  }, {});
-  console.log(selectedPackages);
+  const [sortedPackages] = await Promise.all([
+    fetchJson(
+      "https://dns-exporter.joshuaminer.uk/build_domain_package_list.py",
+      {
+        method: "POST",
+        headers: {
+          "Content-Type": "application/json"
+        },
+        body: JSON.stringify(allPackages)
+      }
+    )
+  ]);
+  console.log(sortedPackages);
   return { status: "done" };
 }
 
